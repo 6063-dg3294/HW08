6d68ba138f36d888abaf0c56a33c571207160221
Difei (Amber) Gao <dg3294@nyu.edu>
2023-11-02 09:55:17 -0400

    2

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let mCamera;
+let nSlider;
 
 function preload() {
   mCamera = loadImage("./Piet.jpeg")
@@ -12,9 +13,15 @@ function setup() {
 
   mCamera.loadPixels();
   noLoop();
+
+  nSlider = createSlider(1, 200, 1, 10);
+  nSlider.position(600, 100);
 }
 
 function draw() {
+  // Slider controls
+  let nscale = nSlider.value();
+
   mCamera.loadPixels();
   background(220, 20, 120);
   for (let vi = 0; vi < mCamera.pixels.length; vi += 4){
